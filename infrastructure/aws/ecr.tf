d8a1840c9af65a5e552e33c6179098e3b85cb095
Robert Jordan <39421615+rojopolis@users.noreply.github.com>
2022-11-09 19:53:57 +0000

    fix(ci): Fix push path.

--- infrastructure/aws/ecr.tf
+++ infrastructure/aws/ecr.tf
@@ -1,5 +1,10 @@
+locals {
+  repos = ["frontend", "backend"]
+}
+
 resource "aws_ecr_repository" "this" {
-  name                 = "lightfeather-devops-code-challenge"
+  for_each             = toset(local.repos)
+  name                 = "lightfeather-devops-code-challenge-${each.value}"
   image_tag_mutability = "MUTABLE"
   encryption_configuration {
     encryption_type = "KMS"
